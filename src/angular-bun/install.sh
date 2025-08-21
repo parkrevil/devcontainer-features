9a5b7c47f529db5e4de78bdadd24fe999942faf3
revil <revil.com@gmail.com>
2025-08-21 14:30:49 +0000

    fix: change angular-bun owner root to remote user

--- src/angular-bun/install.sh
+++ src/angular-bun/install.sh
@@ -2,10 +2,20 @@
 set -e
 
 ANGULAR_VERSION=${VERSION:-"^20"}
+REMOTE_USER=${_REMOTE_USER:-"vscode"}
 
 echo "Installing Angular CLI version $ANGULAR_VERSION using Bun"
 
-bun install -g @angular/cli@"$ANGULAR_VERSION"
+source /etc/profile.d/bun.sh
+
+echo "Using BUN_INSTALL: $BUN_INSTALL"
+
+"$BUN_INSTALL/bin/bun" install -g @angular/cli@"$ANGULAR_VERSION"
+
+chown "$REMOTE_USER:$REMOTE_USER" "$BUN_INSTALL/bin/ng"
+chown -R "$REMOTE_USER:$REMOTE_USER" "$BUN_INSTALL/install/global/node_modules/@angular"
+chown -R "$REMOTE_USER:$REMOTE_USER" "$BUN_INSTALL/install/global/node_modules"
+chown -R "$REMOTE_USER:$REMOTE_USER" "$BUN_INSTALL/bin"
 
 echo "Angular CLI installed successfully"
-echo "bunx ng version: $(ng version)"
+echo "ng version: $(ng version)"
--- test/angular-bun/angular-bun_test.sh
+++ test/angular-bun/angular-bun_test.sh
@@ -3,7 +3,10 @@ set -e
 
 source dev-container-features-test-lib
 
+REMOTE_USER=${_REMOTE_USER:-"vscode"}
+
 check "ng version" bunx ng version
 check "ng location" which ng
+check "ng owned by remote user" ls -la $(which ng) | grep -q "$REMOTE_USER $REMOTE_USER"
 
 reportResults
